93bc17dea3065802d1335dbb4bbc3eee3de51799
AssassinOGX <emil.rodriguez.jimenez@gmail.com>
2024-05-17 18:31:32 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+meme_dict = {"LOL": "una respuesta a algo gracioso", "CRINGE": "algo raro o embarazoso", "ROFL": "una respuesta a una broma", "SHEESH": "ligera desaprobación", "CREEPY": "aterrador, siniestro", "AGGRO": "ponerse agresivo/enojado"}
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+    
+else:
+    print("No tenemos el significado de esa palabra")
